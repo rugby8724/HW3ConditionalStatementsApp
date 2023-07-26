b466208f3d823c5a972083e72db2a2b2ba577c09
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2023-07-26 12:05:16 -0500

    Add project files.

---
+++ HW3ConditionalStatements/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Hello what is your name ");
+
+string firstName  = Console.ReadLine();
+
+if (firstName.ToLower() == "tim")
+{
+    Console.WriteLine("Welcome Professor");
+}
+else
+{
+    Console.WriteLine($"Hello welcome to the class {firstName}");
+}
